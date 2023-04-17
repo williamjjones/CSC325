36e4df0bddf349b229ace67934506ef4994c3e69
William Jones <58607178+williamjjones@users.noreply.github.com>
2023-04-17 18:22:08 -0400

    Added Users into the Demo class to build

--- AirlineTicketingSystemProject/src/utils/Demo.java
+++ AirlineTicketingSystemProject/src/utils/Demo.java
@@ -15,6 +15,16 @@ public class Demo {
 		User user1 = new User("Will", "Jones", "will.jones@gmail.com", "wjones", "Password1");
 		User user2 = new User("Dina", "Frances", "dina.frances@gmail.com", "dfrances", "Password1");
 		User user3 = new User("Phil", "Rogers", "phil.rogers@gmail.com", "progers", "Password1");
+		User user5 = new User("Daniel", "Goldstein", "daniel.goldstein@gmail.com", "dgoldstein", "Password1");
+		User user6 = new User("Tomasz", "Hajman", "tomasz.hajman@gmail.com", "thajman", "Password1");
+		User user7 = new User("Alex", "Lopez", "alez.lopez@gmail.com", "alopez", "Password1");
+		User user8 = new User("Tim", "Sluka", "tim.sluka@gmail.com", "tsluka", "Password1");
+		User user9 = new User("Rob", "Joyce", "rob.joyce@gmail.com", "rjoyce", "Password1");
+		User user10 = new User("Tom", "Thibodeau", "tom.thibodeau@gmail.com", "tthibodeau", "Password1");
+		User user11 = new User("Jim", "Simpson", "jim.simpson@gmail.com", "jsimpson", "Password1");
+		User user12 = new User("Brian", "Martin", "brian.martin@gmail.com", "bmartin", "Password1");
+		User user13 = new User("Willy", "Wonka", "willy.wonka@gmail.com", "wwnka", "Password1");
+		User user14 = new User("Peter", "Parker", "peter.parker@gmail.com", "pparker", "Password1");
 		User admin = User.createAdmin();
 		
 		Airport JFK = new Airport("John F. Kennedy International Airport", "888-888-8888", "JFK", "NY");
